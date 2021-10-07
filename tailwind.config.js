1cf441179dfdf76770fb01f1d5fb2ec18c49b58f
Abigail Kaye Unating <abigailkaye.unating@wvsu.edu.ph>
2021-10-07 14:28:57 +0800

    Configured css

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      "raleway": ["Raleway"],
+      "montserrat": ["Montserrat"],
+    },
+    extend: {
+      inset: {
+        '0': 0,
+        '0.1': '0.05rem',
+        '-0.1': '-0.05rem',
+        '29': '7.06rem',
+        '30': '7.1rem',
+      },
+      colors: {
+        pink: '#FBA7A0',
+        gray: '#7D7D7D'
+      },
+      boxShadow: {
+        pink: '-1px 0px 27px -4px rgba(248,113,101,0.35)',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
