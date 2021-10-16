a746b9449efa7db81787cd7d9d9cb06dc0e21581
Abigail Kaye Unating <abigailkaye.unating@wvsu.edu.ph>
2021-10-16 15:16:58 +0800

    Changed styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,21 @@ module.exports = {
     fontFamily: {
       "raleway": ["Raleway"],
       "montserrat": ["Montserrat"],
+      "inter": ["Inter"]
     },
     extend: {
       inset: {
         '0': 0,
         '0.1': '0.05rem',
         '-0.1': '-0.05rem',
+        '22': '5.5rem',
         '29': '7.06rem',
         '30': '7.1rem',
       },
       colors: {
         pink: '#FBA7A0',
-        gray: '#7D7D7D'
+        gray: '#7D7D7D',
+        dark: '#232323'
       },
       boxShadow: {
         pink: '-1px 0px 27px -4px rgba(248,113,101,0.35)',
